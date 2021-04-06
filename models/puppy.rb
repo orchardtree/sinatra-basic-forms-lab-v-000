eccd8ff3d0256917a3b54fa4fedca1d90aaf206c
Jesse Wenger <wengerj@gmail.com>
2021-04-06 20:25:12 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 class Puppy
   attr_accessor :name, :age
-  attr_read :breed
+  attr_reader :breed
 
 end
 
