3f3796d6a66b805a3e5caf1f6082cb78031208e9
Jesse Wenger <wengerj@gmail.com>
2021-04-06 20:42:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require_relative 'config/environment
-require_relative 'models/puppy.
+require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
   get '/' do
